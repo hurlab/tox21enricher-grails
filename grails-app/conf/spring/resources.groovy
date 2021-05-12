be19be4c4e77c8481f8107ba50319bb9cc845b5e
Parker Combs <41436545+parkerirl@users.noreply.github.com>
2021-05-12 13:58:14 -0500

    Update resources.groovy
    updated pathing for production beans

--- grails-app/conf/spring/resources.groovy
+++ grails-app/conf/spring/resources.groovy
@@ -7,23 +7,23 @@ beans = {
     switch(Environment.current) {
         case Environment.PRODUCTION:
             storageProperties(StorageProperties) {
-                baseDir = "~/tox21enricher/Output/"
-                inputDir = "~/tox21enricher/Input/"
+                baseDir = "${System.properties['user.home']}/tox21enricher/Output/"
+                inputDir = "${System.properties['user.home']}/tox21enricher/Input/"
             }
             scriptLocationProperties(ScriptLocationProperties) {
-                perlScriptDir = "~/tox21enricher/src/main/perl/"
-                pythonScriptDir = "~/tox21enricher/src/main/python/"
+                perlScriptDir = "${System.properties['user.home']}/tox21enricher/src/main/perl/"
+                pythonScriptDir = "${System.properties['user.home']}/tox21enricher/src/main/python/"
             }
             break
 
         case Environment.DEVELOPMENT:
             storageProperties(StorageProperties) {
-                baseDir = "~/tox21enricher/Output/"
-                inputDir = "~/tox21enricher/Input/"
+                baseDir = "${System.properties['user.home']}/tox21enricher/Output/"
+                inputDir = "${System.properties['user.home']}/tox21enricher/Input/"
             }
             scriptLocationProperties(ScriptLocationProperties) {
-                perlScriptDir = "~/tox21enricher/src/main/perl/"
-                pythonScriptDir = "~/tox21enricher/src/main/python/"
+                perlScriptDir = "${System.properties['user.home']}/tox21enricher/src/main/perl/"
+                pythonScriptDir = "${System.properties['user.home']}/tox21enricher/src/main/python/"
             }
             break
     }
